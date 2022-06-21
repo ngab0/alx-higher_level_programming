6506d8d24c1cb5210764f43a9e9edc38a51842c6
ngab0 <jackdonnily@gmail.com>
2022-06-21 22:00:47 +0200

    2nd commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" Second task, on classes and objects"""
+
+class Square:
+    """ Defined by size """
+
+    def __init__(self, size):
+        """ 
+        Args:
+            param1 (size): Description of size
+
+        """
+        self.__size = size
