364fe99bedc720eb62945aa6ec230b0d7011688f
ngab0 <jackdonnily@gmail.com>
2022-09-05 19:19:29 +0200

    10 BugFix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,8 +2,7 @@
 /* a script that computes and prints a factorial */
 function factorial (a) {
   let factorial = 1;
-  if (parseInt(a) === 'NaN' || parseInt(a) === 0) {
-    factorial = 1;
+  if (isNaN(parseInt(a))  || parseInt(a) === 0) {
     console.log(factorial);
   } else {
     for (let i = a; i > 0; i--) {
