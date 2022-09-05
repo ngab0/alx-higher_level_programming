b3024048c900bd76f8a7c135264b9a41550b59eb
ngab0 <jackdonnily@gmail.com>
2022-09-05 15:18:28 +0200

    2-arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*a script that prints a message depending on the number of arguments passed*/
+if (process.argv.length >= 3) {
+	console.log('Argument found');
+} else {
+	console.log('No argument');
+}
+
