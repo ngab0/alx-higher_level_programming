e44f1bea04f3d338eacd9a934e3534961e359a76
ngab0 <jackdonnily@gmail.com>
2022-09-05 15:20:14 +0200

    FixSemiStandartError

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-/*a script that prints a message depending on the number of arguments passed*/
+/* a script that prints a message depending on the number of arguments passed */
 if (process.argv.length >= 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('No argument');
+  console.log('No argument');
 }
-
