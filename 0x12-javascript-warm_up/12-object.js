09101f50f4ef3ef8c7ff3c75e58c52ebc7f1d5ab
ngab0 <jackdonnily@gmail.com>
2022-09-05 19:46:30 +0200

    12

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* update the script to replace value 12 with 89 */
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
