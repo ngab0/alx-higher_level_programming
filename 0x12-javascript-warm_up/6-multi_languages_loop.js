19bae600ee1b08d250ba4bab56df21686ccc780c
ngab0 <jackdonnily@gmail.com>
2022-09-05 16:08:27 +0200

    6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* a script that prints 3 lines by using an array of string and a loop */
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < lines.length; i++) {
+	console.log(lines[i]);
+}
