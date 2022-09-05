da0f7bd0fe8698ee087e34970909c8f4bec0b704
ngab0 <jackdonnily@gmail.com>
2022-09-05 16:01:58 +0200

    5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* a script that prints the first argument converted to an integer */
+if (parseInt(process.argv[2])) {
+	console.log('My number: ' + parseInt(process.argv[2]));
+} else {
+	console.log('Not a number');
+}
