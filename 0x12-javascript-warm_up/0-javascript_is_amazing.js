636146d7a3f1897df0721be64450c7136030a3fe
ngab0 <jackdonnily@gmail.com>
2022-09-05 14:42:17 +0200

    BugFix

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 /* Script that prints “JavaScript is amazing” */
-const myVar = 'Javascript is amazing'
-console.log(myVar)
+const myVar = 'Javascript is amazing';
+console.log(myVar);
