297cac41a9d73ff26df3ad1104a87d1453b04fd1
ngab0 <jackdonnily@gmail.com>
2022-09-05 21:38:19 +0200

    16 & 17

---
+++ 0x12-javascript-warm_up/102-main.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* code to test 102-add_me_maybe */
+const addMeMaybe = require('./102-add_me_maybe').addMeMaybe;
+addMeMaybe(4, function (nb) {
+  console.log('New value: ' + nb);
+});
