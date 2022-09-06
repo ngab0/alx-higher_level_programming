42139df16d8ba29e11d05cd4f208700a695781c7
ngab0 <jackdonnily@gmail.com>
2022-09-06 14:27:13 +0200

    7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/**
+ * a function that returns the number of occurences in a list
+ */
+
+exports.nbOcurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) { count += 1; }
+  }
+  return count;
+};
