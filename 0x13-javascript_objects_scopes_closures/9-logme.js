8ba03d13103eb66216acf42f472ca4b0c6436b76
ngab0 <jackdonnily@gmail.com>
2022-09-07 14:08:54 +0200

    9

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * a function that prints the number of arguments already printed
+ * and the new argument value
+ */
+
+let i = 0;
+exports.logMe = function (item) {
+  console.log(i + ': ' + item);
+  i++;
+};
