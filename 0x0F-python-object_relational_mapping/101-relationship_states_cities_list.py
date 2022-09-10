ee6ee879460ba76b015eed550c0846956c5947e2
ngab0 <jackdonnily@gmail.com>
2022-09-10 23:24:26 +0200

    Advanced

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-changes the name of a State object
-from the database hbtn_0e_6_usa"""
+lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa"""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
 
 
 if __name__ == "__main__":
@@ -18,9 +19,12 @@ if __name__ == "__main__":
                                             ),
                             pool_pre_ping=True
                                 )
-    session = Session(engine)
-    re = session.query(State).filter_by(id=2).first()
-    re.name = "New Mexico"
-    re = session.add(re)
-    session.commit()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    re = session.query(State).all()
+    for x in re:
+        print("{}: {}".format(x.id, x.name))
+        for r in x.cities:
+            print("    {}: {}".format(r.id, r.name))
     session.close()
